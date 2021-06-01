066ce6d911552184daff520b3afc5e3389138055
Hoang Nguyen <hoangnguyen.ncc@gmail.com>
2021-06-01 21:08:02 +0700

    implement data into cart page

--- carts/models.py
+++ carts/models.py
@@ -21,8 +21,8 @@ class CartItem(models.Model):
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
 
-    # def sub_total(self):
-    #     return self.product.price * self.quantity
+    def sub_total(self):
+        return self.product.price * self.quantity
 
     def __unicode__(self):
         return self.product
