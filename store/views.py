9f22db5b04c9f8d386ab81c2cc7a965402e7b85f
Hoang Nguyen <hoangnguyen.ncc@gmail.com>
2021-06-03 21:43:33 +0700

    Two Factor Checks for Submitting Reviews (Login check & Product purchase check)

--- store/views.py
+++ store/views.py
@@ -42,9 +42,24 @@ def product_detail(request, category_slug, product_slug):
         in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e    
+
+    if request.user.is_authenticated:
+        try:
+            orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
+        except OrderProduct.DoesNotExist:
+            orderproduct = None
+    else:
+        orderproduct = None
+    
+    # Get the reviews
+    reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
+
+    
     context = {
         'single_product': single_product,
-        'in_cart': in_cart
+        'in_cart': in_cart,
+        'orderproduct': orderproduct,
+        'reviews': reviews,
     }
     return render(request,'store/product_detail.html', context)
 
