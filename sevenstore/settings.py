b808aa4baf002280c6424accfde6f71d6940cf4f
Hoang Nguyen <hoangnguyen.ncc@gmail.com>
2021-06-05 00:45:13 +0700

    deploying to s3

--- sevenstore/settings.py
+++ sevenstore/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'store',
     'carts',
     'orders',
-    'admin_honeypot'
+    'admin_honeypot',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -88,7 +89,7 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-import os
+
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
@@ -107,6 +108,17 @@ else:
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
+
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': 'ebdb',
+#             'USER': 'tranminhhieu',
+#             'PASSWORD': 'matkhau9xno2',
+#             'HOST': 'aa13nnp8fms9wb7.cwhz1wdhayt4.us-west-2.rds.amazonaws.com',
+#             'PORT': 5432,
+#         }
+# }
 
 
 # Password validation
@@ -145,11 +157,30 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/'static'
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = 'public-read'
+AWS_LOCATION = 'static'
+
 STATICFILES_DIRS = [
     'sevenstore/static',
 ]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+DEFAULT_FILE_STORAGE = 'sevenstore.media_storages.MediaStorage'
+
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR/'static'
+# STATICFILES_DIRS = [
+#     'sevenstore/static',
+# ]
 
 # media files configuration
 MEDIA_URL = '/media/'
